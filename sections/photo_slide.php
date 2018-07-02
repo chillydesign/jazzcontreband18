dfbd46f7238ca50de9b64977b86bc0f15a9b8d64
Charles Harvey <harvey.charles@gmail.com>
2018-07-02 16:34:38 +0200

    added top boxes section

--- sections/photo_slide.php
+++ sections/photo_slide.php
@@ -1,10 +1,8 @@
 <?php $image =  get_sub_field('image'); ?>
-	<?php $background_image =  get_sub_field('background_image'); ?>
+<?php $background_image =  get_sub_field('background_image'); ?>
 
 
- 
-
-	<?php echo get_sub_field('content'); ?>
+<?php echo get_sub_field('content'); ?>
 
 
 	<div class="parallax photo_slide " style="background-image: url(<?php echo $background_image['url']; ?>);"></div>
