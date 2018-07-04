ac3e9f5ea59f38b89ae82e5410bf94fb4404271e
Charles Harvey <harvey.charles@gmail.com>
2018-07-04 23:28:29 +0200

    rearranged downloads

--- sections/downloads.php
+++ sections/downloads.php
@@ -1,18 +1,19 @@
 
 	<div class="container ">
+        <div class="blue_box">
 	<?php if(!empty(get_sub_field('introduction'))){ ?>
 		<div class="introduction"><?php echo get_sub_field('introduction'); ?></div>
 	<?php } ?>
-		<div class="column_container">
+		<div class="column_container download_columns">
 		<?php $i=1; ?>
 		<?php while ( have_rows('documents') ) : the_row(); ?>
 			<div class="column">
 				<p><a target="_blank" href="<?php echo get_sub_field('document'); ?>"><i class="fa fa-download" aria-hidden="true"></i> <?php echo get_sub_field('nom_affiche'); ?></a></p>
 			</div>
-		<?php if($i % 3 == 0){echo '</div><div class="column_container">';} ?>
+		<?php if($i % 3 == 0){echo '</div><div class="column_container download_columns">';} ?>
 		<?php $i++; ?>
 		<?php endwhile; ?>
 		</div> <!-- END OF ROW -->
 
-
+</div>
 	</div><!--  END OF CONTAINER -->
