5c16e3c4d96d1a18b74c80be434217ef5bcca761
Charles Harvey <harvey.charles@gmail.com>
2018-07-04 22:35:57 +0200

    styled galleries

--- gallery_content.php
+++ gallery_content.php
@@ -8,18 +8,18 @@
     <?php $page =  get_sub_field('link'); ?>
     <?php $image =  get_sub_field('image'); ?>
     <div class="column">
-    <a href="<?php echo $page->guid; ?>">
-    <div class=" reperage_box stripes upcoming">
-      <div class="white">
+    <a class="event_gallery" href="<?php echo $page->guid; ?>">
+
+
         <div class="event_thumb" style="padding:30%; background-image:url(<?php echo $image['sizes']['small'] ; ?>);"></div>
         <div class="upcoming_description">
           <h3> <?php echo $page->post_title; ?></h3>
         </div>
-      </div>
-    </div>
+
+
   </a>
   </div>
-  <?php  if($count % 3 == 2) echo '</div> <!-- END OF ROW --> <div class="column_container">' ; ?>
+  <?php  if($count % 2 == 1) echo '</div> <!-- END OF ROW --> <div class="column_container">' ; ?>
 <?php $count++; ?>
 <?php endwhile; ?>
 </div><!-- END OF ROW -->
